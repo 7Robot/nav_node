81cc57f0970ca6fd562c917108ab1c3a88f170d2
Bandamo <bandamo@live.fr>
2023-05-15 17:26:16 +0200

    Ajout des maps individuelles

--- src/map/scriptConvMap.py
+++ src/map/scriptConvMap.py
@@ -1,20 +1,30 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
+import os
 
-a=Image.open("bite.png")
-a=np.asarray(a)
+liste_images = os.listdir("images")
 
-b=np.zeros((200,300))
+for im in liste_images:
+    if not(".png" in im):
+        continue
+    
+    print("Loading : "+im)
 
-for k in range(len(a)):
-    for j in range(len(a[0])):
-            b[k,j] = a[k,j,0]/255
-b=np.logical_not(b)
-b=np.transpose(b)
+    a=Image.open("images/"+im)
+    a=np.asarray(a)
+    print("Shape : "+str(a.shape))
 
-np.save("test_map.npy",b)
-
-plt.imshow(b)
-plt.show()
+    b=np.zeros((200,300))
+    
+    for k in range(len(a)):
+        for j in range(len(a[0])):
+                b[k,j] = a[k,j]==8
+    b=np.logical_not(b)
+    b=np.transpose(b)
+    
+    np.save(im[:-4]+".npy",b)
+    
+    #plt.imshow(b)
+    #plt.show()
 
