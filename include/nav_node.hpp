ff7c06266683a9d91254e5f1fa4923c395500397
Thomas Bonnefille <thomas.bonnefille@outlook.com>
2023-07-09 22:11:28 +0000

    Modifying CMakeLists and Header to include correct libs properly
    Signed-off-by: Thomas Bonnefille <thomas.bonnefille@outlook.com>

--- include/nav_node.hpp
+++ include/nav_node.hpp
@@ -1,11 +1,19 @@
 #ifndef NAV_NODE_HPP
 #define NAV_NODE_HPP
 
+// Standard library
 #include <vector>
 #include "math.h"
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 
+// Nav_node specific
 #include "astar.hpp"
+
+// Messages
+#include "cdf_msgs/Pic_Action.h"
+#include "cdf_msgs/RobotData.h"
+#include "cdf_msgs/MergedDataBis.h"
+#include "cdf_msgs/Trajectoire.h"
 
 #endif // NAV_NODE_HPP
