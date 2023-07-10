f21f11e97e446df83fd258e3ee47474dc81bfd01
Thomas Bonnefille <thomas.bonnefille@outlook.com>
2023-07-10 09:19:03 +0000

    Added basic variables
    Signed-off-by: Thomas Bonnefille <thomas.bonnefille@outlook.com>

--- include/nav_node.hpp
+++ include/nav_node.hpp
@@ -16,4 +16,44 @@
 #include "cdf_msgs/MergedDataBis.h"
 #include "cdf_msgs/Trajectoire.h"
 
+struct Point
+{
+    int x;
+    int y;
+};
+
+class Nav_node
+{
+    public:
+        Nav_node();
+        ~Nav_node();
+
+    private:
+        // ROS
+        ros::NodeHandle n;
+        ros::Subscriber sub_robot_data;
+        ros::Subscriber sub_merged_data;
+        ros::Publisher pub_pic_action;
+        ros::Publisher pub_trajectoire;
+
+        // Callbacks
+        void robot_data_callback(const cdf_msgs::RobotData::ConstPtr& msg);
+        void merged_data_callback(const cdf_msgs::MergedDataBis::ConstPtr& msg);
+
+        // Astar objects
+        Astar astar;
+
+        // Robot data
+        cdf_msgs::RobotData robot_data;
+        Point robot_position;
+        Point robot_goal;
+        
+
+
+
+}
+
+
+
+
 #endif // NAV_NODE_HPP
