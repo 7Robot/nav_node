3841b43a7aa58996da1aebfe3501862374382239
Thomas Bonnefille <thomas.bonnefille@outlook.com>
2023-07-13 08:54:59 +0000

    Add P* Algorithm file
    Signed-off-by: Thomas Bonnefille <thomas.bonnefille@outlook.com>

---
+++ include/pstar.hpp
@@ -0,0 +1,27 @@
+#ifndef PSTAR_HPP
+#define PSTAR_HPP
+
+#include "nav_node.hpp"
+#define MAP_WIDTH 300
+#define MAP_HEIGHT 200
+
+struct Point{
+    int x;
+    int y;
+};
+
+class PStar{
+    public:
+        PStar();
+        ~PStar();
+
+        std::vector<Point> calculate_path(int startX, int startY, int endX, int endY);
+        void set_map(bool map[MAP_WIDTH][MAP_HEIGHT]);
+
+    private:
+        int map_cost[MAP_WIDTH][MAP_HEIGHT];
+        bool map[MAP_WIDTH][MAP_HEIGHT];
+        bool get_cost(Point p);
+};
+
+#endif // PSTAR_HPP
